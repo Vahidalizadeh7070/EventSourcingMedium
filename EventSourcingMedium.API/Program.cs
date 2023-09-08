60a11d367a85a23facba9d1b77e51836043afc70
vahidalizadeh7070 <vahidalizadeh7070@gmail.com>
2023-09-09 01:55:38 +0330

    Add MediatR

--- EventSourcingMedium.API/Program.cs
+++ EventSourcingMedium.API/Program.cs
@@ -2,6 +2,7 @@ using EventSourcingMedium.API.Models;
 using EventSourcingMedium.API.Services.PostInformationServices.CommandService;
 using EventSourcingMedium.API.Services.PostInformationServices.QueryService;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +17,11 @@ builder.Services.AddDbContextPool<AppDbContext>(db => db.UseSqlServer(connection
 builder.Services.AddScoped<IPostInformationCommandService, PostInformationCommandService>();
 builder.Services.AddScoped<IPostInformationQueryService, PostInformationQueryService>();
 
+// Register AutoMapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Register MediatR
+builder.Services.AddMediatR(cfg=>cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
 var app = builder.Build();
 
