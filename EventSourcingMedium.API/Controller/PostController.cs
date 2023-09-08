3fa5dbf0d94086ba2c7bb2920905e514d0ebd2b6
vahidalizadeh7070 <vahidalizadeh7070@gmail.com>
2023-09-08 13:56:52 +0330

    Add Controller to the project

---
+++ EventSourcingMedium.API/Controller/PostController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSourcingMedium.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        public PostController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("GetPost")]
+        public IActionResult Get([FromQuery] string id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+    }
+}
